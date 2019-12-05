ebfb46d09973b59a538680fa7d67e02e82d9a78b
kakeru <okinawan.kkr0713@icloud.com>
2019-12-05 18:02:47 +0900

    削除機能追加

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,11 +15,22 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
   end
+
+  def destroy
+    @agenda = Agenda.find(params[:id])
+    if current_user.agendas || current_user.team.agendas
+      @agenda.destroy
+      redirect_to dashboard_url, notice: I18n.t('views.messages.destroy_agenda')
+    else
+      redirect_to dashboard_url, notice: I18n.t('views.messages.don`t destroy_agenda')
+    end
+  end
+
 
   private
 
